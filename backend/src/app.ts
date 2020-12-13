53136636d16cddb7536ac1e7206b123f11281a4a
Kyle Coberly <kyle.coberly@gmail.com>
2020-12-13 14:55:50 -0700

    Componentizes app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,4 +12,46 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+const videos = [{
+  id: 1,
+  linkUrl: '#',
+  imageUrl: 'images/letterkenny.png',
+  title: 'Letterkenny Season 9 Trailer',
+  description: 'You can watch the new season of Letterkenny December 25th on Crave and December 26th on Hulu if you want to.',
+}, {
+  id: 2,
+  linkUrl: '#',
+  imageUrl: 'images/math-rock.png',
+  title: '5 Awesome FACGCE Riffs - Covet, TTNG, American Football, Colossal',
+  description: 'Hello, Steve here, hope you\'re doing well. In this video, I show you 5 riffs in FACGCE tuning. A question I get asked quite often is what is the best way to learn how to play in other tunings. I think a great way to start is to learn other songs or parts of songs in different tunings, this way you will get some insight into how to possibly play, techniques you can use, where chords are, nice sounding notes and so on.',
+}, {
+  id: 3,
+  linkUrl: '#',
+  imageUrl: 'images/seinfeld.png',
+  title: 'Worst bomb: Eddie Murphy, Norm Macdonald, John Oliver, and Jerry Seinfeld',
+  description: 'John Oliver, Eddie Murphy, Norm Macdonald and Jerry Seinfeld talk about their worst bombs on stage. Cedric The Entertainer talks with Jerry about the joy of talking about other comedians bombing.',
+}, {
+  id: 4,
+  linkUrl: '#',
+  imageUrl: 'images/citra.png',
+  title: 'CITRA - Air (Official Music Video)',
+  description: 'High-energy, guitar driven rock weaving its way through soulful, heartfelt melodies. Bright. Fast. Fun. Loud. CITRA.',
+}, {
+  id: 5,
+  linkUrl: '#',
+  imageUrl: 'images/nathan-fielder.png',
+  title: 'Can the Emmys be Hacked?',
+  description: 'Nathan Fielder reveals potential vulnerabilities in the Emmy online voting system with election security expert Carsten Schürmann.',
+}, {
+  id: 6,
+  linkUrl: '#',
+  imageUrl: 'images/pwas.png',
+  title: 'Progressive Web Apps Are The Future',
+  description: 'Get the lowdown on Progressive Web Apps – why PWAs are the freshest way to reach more users and create richer user experiences and engagement.',
+}];
+
+app.get('/videos', (request: Request, response: Response) => {
+  response.json({ videos });
+});
+
 export default app;
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,18 @@
 <template>
   <div id="the-app">
     <h1>Video Search</h1>
-    <form class="search-form">
-      <input v-model="searchTerm" placeholder="Search for a video" />
-      <font-awesome-icon icon="search" />
-    </form>
+    <SearchForm v-model:searchTerm="searchTerm" />
     <ul>
       <li v-for="searchResult in searchResults" :key="searchResult.id">
-        <div class="video-result">
-          <a href="#"><img src="https://www.schedfast.today/app/webroot/img/video_poster_default.png" alt="" /></a>
-          <section>
-            <h2>{{ searchResult.title }}</h2>
-            <p>{{ searchResult.description }}</p>
-          </section>
-        </div>
+        <VideoResult :video="searchResult" />
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import VideoResult from '@/components/VideoResult.vue';
+import SearchForm from '@/components/SearchForm.vue';
 
 function normalizeString(string) {
   return string.replace(/\s/g, '')
@@ -31,44 +23,22 @@ function normalizeString(string) {
 
 export default {
   name: 'App',
+  components: {
+    VideoResult,
+    SearchForm,
+  },
   data() {
     return {
       searchTerm: '',
-      videos: [{
-        id: 1,
-        imageUrl: 'https://www.schedfast.today/app/webroot/img/video_poster_default.png',
-        title: 'Video',
-        description: 'This is a video description',
-      }, {
-        id: 2,
-        imageUrl: 'https://www.schedfast.today/app/webroot/img/video_poster_default.png',
-        title: 'Video',
-        description: 'This is a video description',
-      }, {
-        id: 3,
-        imageUrl: 'https://www.schedfast.today/app/webroot/img/video_poster_default.png',
-        title: 'Video',
-        description: 'This is a video description',
-      }, {
-        id: 4,
-        imageUrl: 'https://www.schedfast.today/app/webroot/img/video_poster_default.png',
-        title: 'Video',
-        description: 'This is a video description',
-      }, {
-        id: 5,
-        imageUrl: 'https://www.schedfast.today/app/webroot/img/video_poster_default.png',
-        title: 'Video',
-        description: 'This is a video description',
-      }, {
-        id: 6,
-        imageUrl: 'https://www.schedfast.today/app/webroot/img/video_poster_default.png',
-        title: 'Video',
-        description: 'This is a video description',
-      }],
+      videos: [],
     };
   },
-  components: {
-    FontAwesomeIcon,
+  created() {
+    fetch('http://localhost:8091/videos')
+      .then((response) => response.json())
+      .then((response) => {
+        this.videos = response.videos;
+      });
   },
   computed: {
     searchResults() {
@@ -93,32 +63,6 @@ export default {
   padding: $xl;
   h1 {
     @include heading-font-1;
-  }
-  .search-form {
-    margin-bottom: $large;
-    border: 1px solid $grey-9;
-    padding: $small $large;
-    width: 300px;
-    input {
-      border: none;
-    }
-    svg {
-      color: $grey-7;
-    }
-  }
-  .video-result {
-    width: 600px;
-    box-shadow: 0 2px 1px $grey-7;
-    display: grid;
-    grid-template-columns: 150px 1fr;
-    padding: $xl;
-    margin-bottom: $xl;
-    h2 {
-      @include heading-font-2;
-    }
-    section {
-      padding: 0 $large;
-    }
   }
 }
 </style>
